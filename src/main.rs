31c82985195a1050b193d4774656666c9d6a9c90
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-10-26 04:03:59 +0900

    Call SHFileOperationW only once

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,28 @@ const FO_DELETE: u32 = 0x3;
 const FOF_ALLOWUNDO: u16 = 0x40;
 const FOF_NOCONFIRMATION: u16 = 0x10;
 
-fn trash(fname: &str) {
-    let mut source: Vec<u16> = fname.encode_utf16().collect();
-    source.push(0);
-    source.push(0);
-
-    let mut sh_file_op_struct = SHFILEOPSTRUCTW {
-        hwnd: HWND(0),
-        wFunc: FO_DELETE,
-        pFrom: PCWSTR(source.as_mut_ptr()),
-        pTo: w!(""),
-        fFlags: FOF_ALLOWUNDO | FOF_NOCONFIRMATION,
-        fAnyOperationsAborted: BOOL(0),
-        hNameMappings: 0 as *mut c_void,
-        lpszProgressTitle: w!("to trash"),
-    };
-    unsafe {
-        let _ = SHFileOperationW(&mut sh_file_op_struct);
-    }
-}
-
 fn main() {
-    let args = std::env::args();
-    for arg in args.skip(1) {
-        trash(&arg)
+    let mut source: Vec<u16> = Vec::new();
+    for fname in std::env::args().skip(1) {
+        let mut fname_vec: Vec<u16> = fname.encode_utf16().collect();
+        source.append(&mut fname_vec);
+        source.push(0);
+    }
+    if source.len() > 0 {
+        source.push(0);
+        let mut sh_file_op_struct = SHFILEOPSTRUCTW {
+            hwnd: HWND(0),
+            wFunc: FO_DELETE,
+            pFrom: PCWSTR(source.as_mut_ptr()),
+            pTo: w!(""),
+            fFlags: FOF_ALLOWUNDO | FOF_NOCONFIRMATION,
+            fAnyOperationsAborted: BOOL(0),
+            hNameMappings: 0 as *mut c_void,
+            lpszProgressTitle: w!("to trash"),
+        };
+        unsafe {
+            let _ = SHFileOperationW(&mut sh_file_op_struct);
+        }
     }
 }
 
